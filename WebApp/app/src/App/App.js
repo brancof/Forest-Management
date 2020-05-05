0c382c2664eb616d7dfb32b851a0bd446521d1d5
André F <andrecova@users.noreply.github.com>
2020-05-05 15:40:38 +0100

    Update páginas proprietarios

--- WebApp/app/src/App/App.js
+++ WebApp/app/src/App/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import Login from './containers/Login';
-import Register from './containers/Register';
+import {
+//  BrowserRouter,
+//  Switch,
+//  Route,
+//  Link,
+//  Redirect
+} from "react-router-dom";
+//import Login from './containers/Login';
+//import Register from './containers/Register';
+//import Proprietarios from './containers/Proprietarios';
+//import Inspetores from './containers/Inspetores';
+import AppRoutes from "./containers/AppRoutes";
 import './App.css';
 
+
+
 function App() {
+
   return (
     <div className="App">
-      <Register />
+      <AppRoutes />
     </div>
-  );
+  )
 }
 
 export default App;
+
+
